c7edb85ba95c830bb4fd8c1c1e14736d10f26e55
Emre Avsar <emre.avsar92@gmail.com>
2019-11-29 10:22:58 +0100

    Initial commit

---
+++ payments-service/src/test/java/com/ubs/ga0/llcp/payments/PaymentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubs.ga0.llcp.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
